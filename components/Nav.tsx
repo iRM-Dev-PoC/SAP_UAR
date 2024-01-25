ffb035305d5b985dd185d965006b1734eaebf9e6
Suman Mondal <suman.mondal@techriskpartners.com>
2024-01-25 13:40:07 +0530

    fix:

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,39 +1,48 @@
 "use client";
 
-import avater from "@/public/user.png";
 import React, { useState } from "react";
 import { LucideMenu } from "lucide-react";
-import SideNavbar from "./SideNavbar";
 import logout from "@/public/logout.png";
+import img from "@/public/irm.png";
+import { useRouter } from "next/navigation";
+import { ShellBar, Button, Avatar } from "@ui5/webcomponents-react";
+import Image from "next/image";
 
 type navbarprop = { callback: any };
 const NavBar = ({ callback }: navbarprop) => {
-  	const router = useRouter();
+	const router = useRouter();
 
 	const handleLogOut = () => {
 		window.localStorage.clear();
 		router.push("/sign-in");
 	};
 
-  const [isCollapseSidebar, setCollapseSidebar] = useState(true);
-  const handleNavMenuButtonclick = () => {
-    setCollapseSidebar((isCollapseSidebar) => !isCollapseSidebar);
-    callback(isCollapseSidebar);
-  };
+	const [isCollapseSidebar, setCollapseSidebar] = useState(true);
+	const handleNavMenuButtonclick = () => {
+		setCollapseSidebar((isCollapseSidebar) => !isCollapseSidebar);
+		callback(isCollapseSidebar);
+	};
 
-  return (
-    <div className="mb-2 ml-2 mr-2 rounded-xl">
-      <ShellBar
-        startButton={
-          <Button onClick={handleNavMenuButtonclick}>
-            <LucideMenu />
-          </Button>
-        }
-        onProfileClick={handleLogOut}
+	return (
+		<div className="mb-2 ml-2 mr-2 rounded-xl">
+			<ShellBar
+				startButton={
+					<Button onClick={handleNavMenuButtonclick}>
+						<LucideMenu />
+					</Button>
+				}
+				onProfileClick={handleLogOut}
 				onLogoClick={() => router.push("/dashboard")}
-        logo={<Image alt="IRM Logo" src={img} height={32} width={32} />}
-        notificationsCount="10"
-        				profile={
+				logo={
+					<Image
+						alt="IRM Logo"
+						src={img}
+						height={32}
+						width={32}
+					/>
+				}
+				notificationsCount="10"
+				profile={
 					<Avatar>
 						<Image
 							src={logout}
@@ -43,13 +52,13 @@ const NavBar = ({ callback }: navbarprop) => {
 						/>
 					</Avatar>
 				}
-        showNotifications
-        style={{
+				showNotifications
+				style={{
 					borderRadius: "5px !important",
 				}}
 				suppressHydrationWarning></ShellBar>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default NavBar;
